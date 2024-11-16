307b77637e522559022c71239794246315d54908
Akshat Sharma <Akshatsharmain11@gmail.com>
2024-11-17 00:09:30 +0530

    website add

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
+import os
 import google.generativeai as genai
-print("Google Generative AI library is working!")
+
+API_KEY = "AIzaSyBd3rFfaIIwEkRlmQYy2Dbs2iBcgVXefW8";
+genai.configure(api_key=API_KEY)
+
+myfile = genai.upload_file("C:/Users/Aksha/Pictures/nvidia1.png")
+print(f"{myfile=}")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+result = model.generate_content(
+    [myfile, "\n\n", "Can you tell me about the instruments in this photo?"]
+)
+print(result.text)
