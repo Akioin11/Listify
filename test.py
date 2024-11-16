0afe5ddeca2a9a3ac90f84ef20d875b413e1f54d
Akshat Sharma <Akshatsharmain11@gmail.com>
2024-11-17 00:56:38 +0530

    better prompt

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ import google.generativeai as genai
 API_KEY = "AIzaSyBd3rFfaIIwEkRlmQYy2Dbs2iBcgVXefW8";
 genai.configure(api_key=API_KEY)
 
-myfile = genai.upload_file("C:/Users/Aksha/Pictures/nvidia1.png")
+myfile = genai.upload_file("C:/Users/Aksha/Desktop/img1.jpg")
 print(f"{myfile=}")
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 result = model.generate_content(
-    [myfile, "\n\n", "Can you tell me about the instruments in this photo?"]
+    [myfile, "\n\n", "whats the material of the jeans, only mention the most probable one? a lower median price whatever is your best guess? and a 1 line amazon listing title for this"]
 )
 print(result.text)
